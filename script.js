e65b65d828ffe1f410ac8a23802910317de3ee97
Alvar0-20 <alva14200613@gmail.com>
2026-06-11 12:07:39 -0300

    mudado a dinâmica de digitação de valores, agora os valores mínimos tem que ser menores que os valores máximos

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
-    
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert('O valor mínimo tem que ser MENOR que o valor máximo.')
+      }  else {
+            const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(`O número sorteado é: ${result}`);
+            alert(`O número sorteado é: ${result}`);
+        }
+
 }
